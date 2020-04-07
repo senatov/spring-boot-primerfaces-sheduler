8207f6521d931f2aac580187dba997285ffc920e
Iakov Senatov <javaEntwickler@gmail.com>
2020-04-07 02:22:12 +0200

    set security filtering resources on /login

--- src/main/java/de/senatov/reservationz/config/SecurityConfig.java
+++ src/main/java/de/senatov/reservationz/config/SecurityConfig.java
@@ -5,6 +5,7 @@ package de.senatov.reservationz.config;
 import lombok.ToString;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,11 +16,12 @@ import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 @ToString
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.authorizeRequests().antMatchers("/ui/login*", "/ui/loginProcess*", "/ui/logoutSuccess*" ).permitAll();
         http.formLogin()
                 .loginPage("/ui/login")
                 .loginProcessingUrl("/ui/loginProcess")
