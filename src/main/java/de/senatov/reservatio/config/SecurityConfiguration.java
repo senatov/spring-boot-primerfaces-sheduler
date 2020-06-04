71219c0173110dea516c9b3e7476201d6b8fb707
Iakov Senatov <javaEntwickler@gmail.com>
2020-06-04 21:23:13 +0200

    Tested/RT-Error: security.httpBasic().disable() !

---
+++ src/main/java/de/senatov/reservatio/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package de.senatov.reservatio.config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity security) throws Exception
+	{
+		security.httpBasic().disable();
+	}
+}
