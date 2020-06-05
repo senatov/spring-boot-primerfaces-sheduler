295d76547687464603efe2706067b87264302b1a
Iakov Senatov <javaEntwickler@gmail.com>
2020-06-06 01:35:15 +0200

    Tested/RT-Error: the project does remastered

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,3 @@
-INSERT INTO SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME)
-VALUES ('senatov@gamai.de', 'Iakov', 'Senatov', 'senatov');
-
-INSERT INTO SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME)
-VALUES ('ronald.reigan@gamai.de', 'Ronny', 'Gonny', 'ronny');
-
-INSERT INTO SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME)
-VALUES ('george.wn@gamai.de', 'georgiw', 'bush', 'bush');
+INSERT INTO SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME) VALUES ('senatov@gamai.de', 'Iakov', 'Senatov', 'senatov');
+INSERT INTO SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME) VALUES ('ronald.reigan@gamai.de', 'Ronny', 'Gonny', 'ronny');
+INSERT INTO SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME) VALUES ('george.wn@gamai.de', 'georgiw', 'bush', 'bush');
