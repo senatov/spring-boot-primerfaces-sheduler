3b33ec6e9a5667deb37513af505ed9d5a660f2c1
Iakov Senatov <javaEntwickler@gmail.com>
2020-06-15 15:37:52 +0200

    not tested: config the MVC logik

--- src/main/java/de/senatov/reservatio/controller/UIController.java
+++ src/main/java/de/senatov/reservatio/controller/UIController.java
@@ -2,52 +2,55 @@ package de.senatov.reservatio.controller;
 
 
 
-import de.senatov.reservatio.db.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
 @Controller
 public class UIController {
 
-	@GetMapping("/reservation")
-	public String uigreeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+	@GetMapping("/{firstValue}")
+	public String requestWithError(
+			@PathVariable("firstValue")
+					String firstValue) {
 
-		model.addAttribute("name", name);
-		return "greeting Reservation!";
+		return firstValue;
 	}
 
-	@RequestMapping(value = "/reservation", method = RequestMethod.POST)
-	public String uisubmit(@ModelAttribute("company") final User user, final BindingResult result, final ModelMap model) {
-		if (result.hasErrors()) {
-			return "error";
-		}
-		model.addAttribute("name", user.getUsername());
-		model.addAttribute("id", user.getId());
-		//companyMap.put(company.getId(), company);
-		return "companyView";
-	}
+    @GetMapping("/{firstValue}/{secondValue}")
+    public String requestWithError(
+            @PathVariable("firstValue")
+                    String firstValue,
+            @PathVariable("secondValue")
+                    String secondValue) {
 
-	@GetMapping("/ui/reservation")
-	public String sgreeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        return firstValue + " - " + secondValue;
+    }
 
-		model.addAttribute("name", name);
-		return "greeting Reservation!";
-	}
 
-	@RequestMapping(value = "/ui/reservation", method = RequestMethod.POST)
-	public String ssubmit(@ModelAttribute("company") final User user, final BindingResult result, final ModelMap model) {
-		if (result.hasErrors()) {
-			return "error";
-		}
-		model.addAttribute("name", user.getUsername());
-		model.addAttribute("id", user.getId());
-		//companyMap.put(company.getId(), company);
-		return "companyView";
-	}
+
+    @GetMapping("/{firstValue}/{secondValue:.+}")
+    public String requestWithRegex(
+            @PathVariable("firstValue")
+                    String firstValue,
+            @PathVariable("secondValue")
+                    String secondValue) {
+
+        return firstValue + " - " + secondValue;
+    }
+
+
+
+    @GetMapping("/{firstValue}/{secondValue}/")
+    public String requestWithSlash(
+            @PathVariable("firstValue")
+                    String firstValue,
+            @PathVariable("secondValue")
+                    String secondValue) {
+
+        return firstValue + " - " + secondValue;
+    }
 
 }
