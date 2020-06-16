8881f16dbaf61d98d68cfc7128307a2514b0fcb8
Iakov Senatov <javaEntwickler@gmail.com>
2020-06-16 16:09:04 +0200

    tested: problem with favicons

--- src/main/java/de/senatov/reservatio/controller/UIController.java
+++ src/main/java/de/senatov/reservatio/controller/UIController.java
@@ -11,34 +11,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UIController {
 
-    @GetMapping("/{firstValue}")
+    @GetMapping("/")
     public String requestWithError(@PathVariable("firstValue") String firstValue) {
 
-        return "ui/reservation.xhtml";
+        return "/ui/reservation";
     }
 
 
 
-    @GetMapping("/{firstValue}/{secondValue}")
-    public String requestWithError(@PathVariable("firstValue") String firstValue, @PathVariable("secondValue") String secondValue) {
+    @GetMapping("/{firstValue}")
+    public String requestOneWithError(@PathVariable("firstValue") String firstValue) {
 
-        return "ui/reservation.xhtml";
+        return "/ui/reservation";
     }
 
 
 
-    @GetMapping("/{firstValue}/{secondValue:.+}")
-    public String requestWithRegex(@PathVariable("firstValue") String firstValue, @PathVariable("secondValue") String secondValue) {
+    @GetMapping("/ui/reservation/{page}")
+    public String requesWOError(@PathVariable("page") String page) {
 
-        return "ui/reservation.xhtml";
+        return "/ui/reservation";
     }
 
 
 
-    @GetMapping("/{firstValue}/{secondValue}/")
-    public String requestWithSlash(@PathVariable("firstValue") String firstValue, @PathVariable("secondValue") String secondValue) {
+    @GetMapping("/ui/{secondValue:.+}")
+    public String requestWithRegex(@PathVariable("secondValue") String secondValue) {
 
-        return "ui/reservation.xhtml";
+        return "404.xhtml";
+    }
+
+
+
+    @GetMapping("/ui/{secondValue}/")
+    public String requestWithSlash(@PathVariable("secondValue") String secondValue) {
+
+        return "/ui/reservation";
     }
 
 }
