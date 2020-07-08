6f9a1bcd74df45da218eb37f94674abb69ca4880
Iakov Senatov <javaEntwickler@gmail.com>
2020-07-08 20:05:03 +0200

    ok/teste: polish

--- src/main/java/de/senatov/reservatio/utl/SchedUtilility.java
+++ src/main/java/de/senatov/reservatio/utl/SchedUtilility.java
@@ -6,8 +6,6 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.stereotype.Component;
--- src/test/java/de/senatov/reservatio/test/IntegrationTest.java
+++ src/test/java/de/senatov/reservatio/test/IntegrationTest.java
@@ -4,7 +4,6 @@ package de.senatov.reservatio.test;
 
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
