fbca74f337e7d97dfc2b3ca8f9d51c7259452316
Iakov Senatov <javaEntwickler@gmail.com>
2020-07-12 00:09:55 +0200

    ok: + add master data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,27 +7,27 @@ INSERT INTO schedule_db.SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME)
 VALUES ('george.wn@gamai.de', 'georgiw', 'bush', 'bush');
 
 INSERT INTO schedule_db.sc_schedule (description, start_date, end_date, group_id, is_editable, schedule_id, style_class, title, url, user_name_id)
-VALUES ('room123-descr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'gr1', true, '1',
+VALUES ('room123-descr', to_timestamp(10000), to_timestamp(10000), 'gr1', true, '1',
         '2', 'room123-title', 'url1', null);
 
 INSERT INTO schedule_db.sc_schedule (description, start_date, end_date, group_id, is_editable, schedule_id, style_class, title, url, user_name_id)
-VALUES ('WXC Lab 2-descr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'gr2', true, '2',
+VALUES ('WXC Lab 2-descr', to_timestamp(20000), to_timestamp(20000), 'gr2', true, '2',
         '2', 'WXC Lab 2-title', 'url1', null);
 
 INSERT INTO schedule_db.sc_schedule (description, start_date, end_date, group_id, is_editable, schedule_id, style_class, title, url, user_name_id)
-VALUES ('WXC Lab-descr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'gr3', true, '3',
+VALUES ('WXC Lab-descr', to_timestamp(30000), to_timestamp(30000), 'gr3', true, '3',
         '2', 'WXC Lab-title', 'url1', null);
 
 INSERT INTO schedule_db.sc_schedule (description, start_date, end_date, group_id, is_editable, schedule_id, style_class, title, url, user_name_id)
-VALUES ('room123-descr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'gr4', true, '4',
+VALUES ('room123-descr', to_timestamp(40000), to_timestamp(40000), 'gr4', true, '4',
         '2', 'room123-title', 'url1', null);
 
 INSERT INTO schedule_db.sc_schedule (description, start_date, end_date, group_id, is_editable, schedule_id, style_class, title, url, user_name_id)
-VALUES ('Kantine-descr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'gr4', true, '5',
+VALUES ('Kantine-descr', to_timestamp(50000), to_timestamp(50000), 'gr4', true, '5',
         '2', 'Kantine-title', 'url1', null);
 
 INSERT INTO schedule_db.sc_schedule (description, start_date, end_date, group_id, is_editable, schedule_id, style_class, title, url, user_name_id)
-VALUES ('Meeting Room 34-descr', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'gr4', true, '5',
+VALUES ('Meeting Room 34-descr', to_timestamp(60000), to_timestamp(60000), 'gr4', true, '5',
         '2', 'Meeting Room 34-title', 'url1', null);
 
 update schedule_db.sc_schedule set user_name_id=( select u.id from schedule_db.sc_user u where user_name like '%senatov%') where title like ('%room123%');
