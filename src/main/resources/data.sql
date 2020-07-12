b2bf27be4ec147f8e3b8290886e0a9e0120ab965
Iakov Senatov <javaEntwickler@gmail.com>
2020-07-12 22:12:03 +0200

    ok/tested yet: add master data script for 2 tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 truncate TABLE schedule_db.sc_user cascade;
+
 INSERT INTO schedule_db.SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME)
 VALUES ('senatov@gamai.de', 'Iakov', 'Senatov', 'senatov');
 INSERT INTO schedule_db.SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME)
--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO schedule_db.SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME) VALUES ('senatov@gamai.de', 'Iakov', 'Senatov', 'senatov');
-INSERT INTO schedule_db.SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME) VALUES ('ronald.reigan@gamai.de', 'Ronny', 'Gonny', 'ronny');
-INSERT INTO schedule_db.SC_USER (E_MAIL, FIRST_NAME, LAST_NAME, USER_NAME) VALUES ('george.wn@gamai.de', 'georgiw', 'bush', 'bush');
