deb874f69ff6ba84a934d97291e8138772b524ed
i.senatov <javaentwickler@gmail.com>
2021-11-02 00:15:09 +0100

    button added on the top panel

--- src/main/java/de/senatov/reservatio/testtest/InputTextView.java
+++ src/main/java/de/senatov/reservatio/testtest/InputTextView.java
@@ -1,5 +1,21 @@
 package de.senatov.reservatio.testtest;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+
+@Named
+@RequestScoped
 public class InputTextView {
 
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
 }
